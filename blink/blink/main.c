e498eac5052bed1e0e63eaddbe5281b4e3e5a8b3
Typical Username <42663452+Typcial-Username@users.noreply.github.com>
2025-01-23 18:13:31 -0700

    initial Commit

---
+++ blink/blink/main.c
@@ -0,0 +1,20 @@
+/*
+ * blink.c
+ *
+ * Created: 1/23/2025 6:09:13 PM
+ * Due: 1/29/25
+ * Author : Levi Terry
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	
+    
+    while (1) 
+    {
+    }
+}
+
