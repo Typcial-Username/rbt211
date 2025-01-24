7430512c6259ff348bf92f60f1b1c46dda852767
Typical Username <42663452+Typcial-Username@users.noreply.github.com>
2025-01-23 18:23:32 -0700

    should blink - need port

--- blink/blink/main.c
+++ blink/blink/main.c
@@ -6,15 +6,24 @@
  * Author : Levi Terry
  */ 
 
+// Constants
+#define F_CPU 16000000
+
+// Built-in Libraries
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Main Loop
 int main(void)
 {
-	
+	DDRB |= 0b00100000; // OR 0x20 OR 32
     
     while (1) 
     {
+		PORTB |= 0b00100000;
+		_delay_ms(500);
+		
+		PORTB &= ~(0b00100000);
     }
 }
 
